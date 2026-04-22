ba7c8e1bc0c8e37da0acfcf2dbf7e3fe8a8605cd
juan-gallo <juanfgallo@experimentality.co>
2026-04-21 22:40:51 -0500

    creating conectivity tests with hugging face

--- tests/infrastructure/out/hugging_face/test_hf_client_integration.py
+++ tests/infrastructure/out/hugging_face/test_hf_client_integration.py
@@ -1,11 +1,25 @@
 from infrastructure.out.hugging_face.hf_client import build_hf_api
 from infrastructure.out.secrets_config.hf_token_config import HFTokenClass
 
-def test_call_hf_api():
+from itertools import islice
+
+def test_inspect_co2_ranges():
     token = HFTokenClass().get_hf_token_from_call
     hf_api = build_hf_api(token=token)
 
-    models = list(hf_api.list_models(limit=1))
+    models = list(
+        islice(
+            hf_api.list_models(
+                emissions_thresholds=(0, None),
+                cardData=True,
+            ),
+            10,
+        )
+    )
 
-    assert hf_api is not None
-    assert len(models) == 1
+    for model in models:
+        print(model.id)
+        print(model.cardData)
+        print("-----")
+
+    assert models
