c538039f4d9a01f2213e7d89074a3b5c4756ab63
juan-gallo <juanfgallo@experimentality.co>
2026-05-05 19:17:50 -0500

    moddifying notebook

--- tests/infrastructure/out/hugging_face/test_hf_client_integration.py
+++ tests/infrastructure/out/hugging_face/test_hf_client_integration.py
@@ -7,15 +7,14 @@ def test_inspect_co2_ranges():
     token = HFTokenClass().get_hf_token_from_call
     hf_api = build_hf_api(token=token)
 
-    models = list(
-        islice(
-            hf_api.list_models(
-                emissions_thresholds=(0, None),
-                cardData=True,
-            ),
-            10,
-        )
+    models_iter = hf_api.list_models(
+        emissions_thresholds=(0, None),
+        cardData=True,
     )
+
+    models = list(islice(models_iter, 10))
+
+    models2 = list(islice(models_iter, 10))
 
     for model in models:
         print(model.id)
