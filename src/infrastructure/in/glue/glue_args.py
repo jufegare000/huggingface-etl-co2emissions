350846e9779c751dbb88cb6d2834843160f18e81
juan-gallo <juanfgallo@experimentality.co>
2026-05-14 19:03:10 -0500

    removing unused arguments

--- src/infrastructure/in/glue/glue_args.py
+++
@@ -1,6 +0,0 @@
-import sys
-
-def get_glue_args(required_args):
-    # noinspection PyUnresolvedReferences
-    from awsglue.utils import getResolvedOptions
-    return getResolvedOptions(sys.argv, required_args)
